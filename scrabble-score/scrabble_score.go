00ec20a4afa807075e814bdd5f736e9ff24c7f8c
Daniel Andrade <daniel@danielandrade.net>
2019-03-17 10:59:24 +0000

    Final scrabble code

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,7 +1,21 @@
 // Package scrabble provides all the tools for the scrabble exercism task.
 package scrabble
 
-import "strings"
+import "unicode"
+
+// Benchmark results with code improvements
+//
+// Initially with map inside function:
+// BenchmarkScore-8          100000             16328 ns/op            6384 B/op         45 allocs/op
+//
+// Moving map outside function:
+// BenchmarkScore-8         1000000              1106 ns/op              32 B/op          4 allocs/op
+//
+// Changing strings.ToLower to unicode.ToLower
+// BenchmarkScore-8         2000000               985 ns/op               0 B/op          0 allocs/op
+//
+// Using switch instead of map
+// BenchmarkScore-8         5000000               327 ns/op               0 B/op          0 allocs/op
 
 var points = map[rune]int{
 	'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
@@ -13,15 +27,34 @@ var points = map[rune]int{
 	'q': 10, 'z': 10,
 }
 
+// getScores returns the scrabble value for a given letter
+func getScores(letter rune) int {
+	switch letter {
+	case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+		return 1
+	case 'd', 'g':
+		return 2
+	case 'b', 'c', 'm', 'p':
+		return 3
+	case 'f', 'h', 'v', 'w', 'y':
+		return 4
+	case 'k':
+		return 5
+	case 'j', 'x':
+		return 8
+	case 'q', 'z':
+		return 10
+	default:
+		return 0
+	}
+}
+
 // Score calculates and returns the scrabble score of a given word
 func Score(word string) int {
-
 	score := 0
-	word = strings.ToLower(word)
-
 	for _, letter := range word {
-		score += points[letter]
+		letter = unicode.ToLower(letter)
+		score += getScores(letter)
 	}
-
 	return score
 }
