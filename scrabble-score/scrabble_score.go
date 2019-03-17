1a0ddf5f34db7c5ab903d3a009d5489acba41590
Daniel Andrade <daniel@danielandrade.net>
2019-03-17 20:30:54 +0000

    Finished code

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -3,30 +3,6 @@ package scrabble
 
 import "unicode"
 
-// Benchmark results with code improvements
-//
-// Initially with map inside function:
-// BenchmarkScore-8          100000             16328 ns/op            6384 B/op         45 allocs/op
-//
-// Moving map outside function:
-// BenchmarkScore-8         1000000              1106 ns/op              32 B/op          4 allocs/op
-//
-// Changing strings.ToLower to unicode.ToLower
-// BenchmarkScore-8         2000000               985 ns/op               0 B/op          0 allocs/op
-//
-// Using switch instead of map
-// BenchmarkScore-8         5000000               327 ns/op               0 B/op          0 allocs/op
-
-var points = map[rune]int{
-	'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
-	'd': 2, 'g': 2,
-	'b': 3, 'c': 3, 'm': 3, 'p': 3,
-	'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
-	'k': 5,
-	'j': 8, 'x': 8,
-	'q': 10, 'z': 10,
-}
-
 // getScores returns the scrabble value for a given letter
 func getScores(letter rune) int {
 	switch letter {
